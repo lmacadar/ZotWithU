088a4a25fb94fc6fd88e208f594003b382f6c984
sudo-omar <omarabouharb2805@gmail.com>
2024-01-26 19:23:01 -0800

    boilerplate

---
+++ ZotWithYou/ZotWithYou/ZotWithYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZotWithYouApp.swift
+//  ZotWithYou
+//
+//  Created by Omar Abou-Harb on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ZotWithYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
