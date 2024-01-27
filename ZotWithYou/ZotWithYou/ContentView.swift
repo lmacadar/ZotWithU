6e30eef9c3faa831d838de46cad709bc4ef59dec
LiesDespair <alexstandards101@gmail.com>
2024-01-27 09:15:49 -0800

    testBlock

--- ZotWithYou/ZotWithYou/ContentView.swift
+++ ZotWithYou/ZotWithYou/ContentView.swift
@@ -13,14 +13,33 @@ struct ContentView: View {
     @State private var email: String = ""
     @State private var password: String = ""
 
+    @State private var blockButtonText: String = "Block"
+    @State private var isBlocked: Bool = false
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
-                .imageScale(.large)
+                //.imageScale(.large)
                 .foregroundStyle(.tint)
-            TextField("Enter your name", text: $name)
-            TextField("Enter your email", text: $email)
-            SecureField("Enter your password", text: $password)
+                .aspectRatio(contentMode: .fill) //images to fill
+
+            TextField("Enter your name", text: $name) //name
+	            .frame(width: 200, height: 40) 
+            TextField("Enter your email", text: $email) //email
+	            .frame(width: 100, height: 40) 
+            SecureField("Enter your password", text: $password) //password
+	            .frame(width: 50, height: 40) 
+
+            Button(blockButtonText) {
+                if(isBlocked == false){
+                    blockButtonText = "Unblock"
+                    isBlocked = true
+                }else{
+                    blockButtonText = "Block"
+                    isBlocked = false
+                }
+            }
+            
         }
         .padding()
     }
