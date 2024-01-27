ded99dfa1920e8e5ee7baa8a08eb5dd75754b087
LiesDespair <alexstandards101@gmail.com>
2024-01-27 14:58:09 -0800

    locationMissingStuff
    omar: time
    luca: images and descriptions

--- ZotWithYou/ZotWithYou/Club.swift
+++ ZotWithYou/ZotWithYou/Club.swift
@@ -16,40 +16,18 @@ struct Club: View{
     @State private var clubTimes = "t/est"
     @State private var clubTags: [String] = ["TEST", "TEST", "TEST"]
 
-    @State private var smallTextSize: Int = 12 
-    @State private var mediumTextSize: Int = 16 
-    @State private var largeTextSize: Int = 20 
-    
     var body: some View{
         VStack{
             Text(clubName)
-                .font(.system(size: largeTextSize))
-                .padding()
             Text("Message Board")
-                .font(.system(size: mediumTextSize))
-                .padding()
             Text(clubBoard)
-                .font(.system(size: smallTextSize))
-                .padding()
             Text("Info:")
-                .font(.system(size: mediumTextSize))
-                .padding()
             Text(clubInfo)
-                .font(.system(size: smallTextSize))
-                .padding()
             Text("Meeting Times:")
-                .font(.system(size: mediumTextSize))
-                .padding()
             Text(clubTimes)
-                .font(.system(size: smallTextSize))
-                .padding()
             Text("Tags:")
-                .font(.system(size: mediumTextSize))
-                .padding()
             let clubTagsString = clubTags.joined(separator: ", ")
             Text(clubTagsString)
-                .font(.system(size: smallTextSize))
-                .padding()
         }
         .padding()
     }
--- ZotWithYou/ZotWithYou/Location.swift
+++ ZotWithYou/ZotWithYou/Location.swift
@@ -13,25 +13,17 @@ struct Location: View{
     @State private var locationName: [String] = ["Einstein Bros. Bagels", "Phoenix Food Court", "Java City Coffee", "Green Room", "Med Ed Cafe", "Anthill Pub & Grille", "Bento Sushi", "Jamba Juice", "Subway", "Starbucks (Student Center)", "Starbucks (Paul Merage)", "Tortilla Fresca"]
     @State private var locationAddress: [String] = ["6318 Student Center Building, Irvine", "Inner Ring Road", "2005 Los Trancos Drive, Irvine", "4000 Mesa Road, Irvine", "1200 A Medical Education Building, Irvine", "215 Pereira Drive, Irvine", "113 Pereira Drive, Irvine", "311A Student Center, Irvine", "A233 Student Center Food Court, Irvine", "203 B Student Center, Irvine", "4255 Campus Drive, Irvine", "Phoenix Food Court, Irvine"]
     @State private var selectedLocation = ""
+    @State private var selectedTime = ""
 
-    @State private var smallTextSize: Int = 12 
-    @State private var mediumTextSize: Int = 16 
-    @State private var largeTextSize: Int = 20 
-
+    //there are 12 locations/addresses that share the same index but different array
     //luca you will edit the empty texts with descriptions, I labeled it "reallyblank" for easy search
     //luca you will also add images into "alsoblank"
     
     var body: some View{
         VStack{
             Text("\(locationName[0]): \(locationAddress[0])")
-                .font(.system(size: largeTextSize))
-                .padding()
             Text("Description: ")
-                .font(.system(size: mediumTextSize))
-                .padding()
             Text("reallyblank") 
-                .font(.system(size: smallTextSize))
-                .padding()
             Button("Select: \(locationName[0])"){
                 selectedLocation = "\(locationName[0])"
             }
@@ -40,8 +32,136 @@ struct Location: View{
                 //.foregroundStyle(.tint)
                 //.aspectRatio(contentMode: .fill) //images to fill
         //-------------------------------------------------------------
+            Text("\(locationName[1]): \(locationAddress[1])")
+            Text("Description: ")
+            Text("reallyblank") 
+            Button("Select: \(locationName[1])"){
+                selectedLocation = "\(locationName[1])"
+            }
+            Image(systemName: "alsoblank")
+                //.imageScale(.large)
+                //.foregroundStyle(.tint)
+                //.aspectRatio(contentMode: .fill) //images to fill
+        //-------------------------------------------------------------
+            Text("\(locationName[2]): \(locationAddress[2])")
+            Text("Description: ")
+            Text("reallyblank") 
+            Button("Select: \(locationName[2])"){
+                selectedLocation = "\(locationName[2])"
+            }
+            Image(systemName: "alsoblank")
+                //.imageScale(.large)
+                //.foregroundStyle(.tint)
+                //.aspectRatio(contentMode: .fill) //images to fill
+        //-------------------------------------------------------------
+            Text("\(locationName[3]): \(locationAddress[3])")
+            Text("Description: ")
+            Text("reallyblank") 
+            Button("Select: \(locationName[3])"){
+                selectedLocation = "\(locationName[3])"
+            }
+            Image(systemName: "alsoblank")
+                //.imageScale(.large)
+                //.foregroundStyle(.tint)
+                //.aspectRatio(contentMode: .fill) //images to fill
+        //-------------------------------------------------------------
+            Text("\(locationName[4]): \(locationAddress[4])")
+            Text("Description: ")
+            Text("reallyblank") 
+            Button("Select: \(locationName[4])"){
+                selectedLocation = "\(locationName[4])"
+            }
+            Image(systemName: "alsoblank")
+                //.imageScale(.large)
+                //.foregroundStyle(.tint)
+                //.aspectRatio(contentMode: .fill) //images to fill
+        //-------------------------------------------------------------
+            Text("\(locationName[5]): \(locationAddress[5])")
+            Text("Description: ")
+            Text("reallyblank") 
+            Button("Select: \(locationName[5])"){
+                selectedLocation = "\(locationName[5])"
+            }
+            Image(systemName: "alsoblank")
+                //.imageScale(.large)
+                //.foregroundStyle(.tint)
+                //.aspectRatio(contentMode: .fill) //images to fill
+        //-------------------------------------------------------------
+            Text("\(locationName[6]): \(locationAddress[6])")
+            Text("Description: ")
+            Text("reallyblank") 
+            Button("Select: \(locationName[6])"){
+                selectedLocation = "\(locationName[6])"
+            }
+            Image(systemName: "alsoblank")
+                //.imageScale(.large)
+                //.foregroundStyle(.tint)
+                //.aspectRatio(contentMode: .fill) //images to fill
+        //-------------------------------------------------------------
+            Text("\(locationName[7]): \(locationAddress[7])")
+            Text("Description: ")
+            Text("reallyblank") 
+            Button("Select: \(locationName[7])"){
+                selectedLocation = "\(locationName[7])"
+            }
+            Image(systemName: "alsoblank")
+                //.imageScale(.large)
+                //.foregroundStyle(.tint)
+                //.aspectRatio(contentMode: .fill) //images to fill
+        //-------------------------------------------------------------
+            Text("\(locationName[8]): \(locationAddress[8])")
+            Text("Description: ")
+            Text("reallyblank") 
+            Button("Select: \(locationName[8])"){
+                selectedLocation = "\(locationName[8])"
+            }
+            Image(systemName: "alsoblank")
+                //.imageScale(.large)
+                //.foregroundStyle(.tint)
+                //.aspectRatio(contentMode: .fill) //images to fill
+        //-------------------------------------------------------------
+            Text("\(locationName[9]): \(locationAddress[9])")
+            Text("Description: ")
+            Text("reallyblank") 
+            Button("Select: \(locationName[9])"){
+                selectedLocation = "\(locationName[9])"
+            }
+            Image(systemName: "alsoblank")
+                //.imageScale(.large)
+                //.foregroundStyle(.tint)
+                //.aspectRatio(contentMode: .fill) //images to fill
+        //-------------------------------------------------------------
+            Text("\(locationName[10]): \(locationAddress[10])")
+            Text("Description: ")
+            Text("reallyblank") 
+            Button("Select: \(locationName[10])"){
+                selectedLocation = "\(locationName[10])"
+            }
+            Image(systemName: "alsoblank")
+                //.imageScale(.large)
+                //.foregroundStyle(.tint)
+                //.aspectRatio(contentMode: .fill) //images to fill
+        //-------------------------------------------------------------
+            Text("\(locationName[11]): \(locationAddress[11])")
+            Text("Description: ")
+            Text("reallyblank") 
+            Button("Select: \(locationName[11])"){
+                selectedLocation = "\(locationName[11])"
+            }
+            Image(systemName: "alsoblank")
+                //.imageScale(.large)
+                //.foregroundStyle(.tint)
+                //.aspectRatio(contentMode: .fill) //images to fill
+        //-------------------------------------------------------------
+        //-------------------------------------------------------------
+        //-------------------------------------------------------------
+            Text("Select a time for \(selectedLocation)")
 
-
+            //omar mess with the time and make sure timeSelected variable is used with button
+            
+            Button("Confirm \(time)"){
+                
+            }
         }
         .padding()
     }
